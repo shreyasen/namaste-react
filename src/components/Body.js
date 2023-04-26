dd639f279ea247eed844125e613641ccf3f3acfe
Shreya <ssen20170@gmail.com>
2023-04-26 17:13:13 +0530

    feat: chapter 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import restaurants from "../../restaurants.json";
 import RestaurantCard from "./RestaurantCard";
 
 const Body = () => {
-  const [restaurantList, setRestaurantList] = useState(restaurants);
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [filteredRestaurantList, setFilteredRestaurantList] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   const filterData = (searchTxt) => {
-    const filteredList = restaurants.filter((res) => {
+    const filteredList = restaurantList.filter((res) => {
       return res.data.name.toLowerCase().includes(searchTxt.toLowerCase());
     });
-    setRestaurantList(filteredList);
+    setFilteredRestaurantList(filteredList);
   };
 
+  const getRestaurants = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.6739913&lng=88.35064679999999&page_type=DESKTOP_WEB_LISTING"
+    );
+    const json = await data.json();
+    console.log(json);
+    setRestaurantList(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurantList(json?.data?.cards[2]?.data?.data?.cards);
+  };
+
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  if (!restaurantList.length) {
+    return <h1>loading...</h1>;
+  }
   return (
     <>
       <div className="search-container">
@@ -31,7 +49,10 @@ const Body = () => {
         </button> */}
       </div>
       <div className="restaurant-list">
-        {restaurantList.map((restaurant) => {
+        {restaurantList.length > 0 && filteredRestaurantList.length === 0 && (
+          <h1>No restaurant found!</h1>
+        )}
+        {filteredRestaurantList.map((restaurant) => {
           // we are passing props by destructuring the elements of restaurants.data
           return (
             <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
